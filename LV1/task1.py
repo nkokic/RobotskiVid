101171868b0dcaf2cc025ed57eb9100f5ee5863f
nkokic <nikola1.kokic@gmail.com>
2025-05-15 19:30:24 +0200

    LV1 done

--- LV1/task1.py
+++ LV1/task1.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import json
+import matplotlib.pyplot as plt
 
 cap = cv2.VideoCapture(0)
 
@@ -66,6 +67,27 @@ while True:
     if len(points) >= 6:
         break
 
+canny1 = canny_capture.copy()
+
+lines = cv2.HoughLines(canny_capture, 1, np.pi / 180, 100)
+
+if lines is not None:
+    for i in range(0, len(lines)):
+        rho = lines[i][0][0]
+        theta = lines[i][0][1]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+        pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+        cv2.line(canny1, pt1, pt2, (255,255,255), 3, cv2.LINE_AA)
+
+cv2.imshow("Houghlines1", canny1)
+cv2.waitKey()
+
+print(lines)
+
 # 250 x 170
 worldPoints = np.array([[0, 0, 0], [0, 0.17, 0], [0.25, 0, 0], [0.25, 0.17, 0]])
 
@@ -79,8 +101,8 @@ rotation, _ = cv2.Rodrigues(rotation)
 A = camera_matrix @ rotation
 b = camera_matrix @ position
 
-theta = 0
-ro = 1
+theta = lines[0,0,0]
+ro = lines[0,0,1]
 
 lambdaX = A[0,0] * np.cos(theta) + A[1,0] * np.sin(theta) - ro * A[2,0]
 lambdaY = A[0,1] * np.cos(theta) + A[1,1] * np.sin(theta) - ro * A[2,2]
@@ -89,5 +111,16 @@ lambdaRo = b[2] * ro - b[0] * np.cos(theta) - b[1] * np.sin(theta)
 newTheta = np.atan2(lambdaY, lambdaX)
 newRo = lambdaRo / np.sqrt(lambdaX**2 + lambdaY**2)
 
-print(f"theta: {newTheta}")
-print(f"ro: {newRo}")
+a = -np.cos(newTheta)/np.sin(newTheta)
+b = newRo/np.sin(newTheta)
+
+print(f"a: {a}")
+print(f"b: {b}")
+
+angle = np.rad2deg(np.atan(a))
+ref_angle = np.rad2deg(np.atan(7/14))
+
+print(f"angle: {angle}°")
+print(f"ref angle: {ref_angle}°")
+
+print(f"delta angle: {np.abs(angle-ref_angle)}°")
